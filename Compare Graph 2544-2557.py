c3de948ccc0a554765afeee18c5dd03c2a0d8c6e
Nuengnakhap <31353598+Nuengnakhap@users.noreply.github.com>
2017-11-22 15:56:38 +0700

    Update Compare Graph 2544-2557.py

--- Compare Graph 2544-2557.py
+++ Compare Graph 2544-2557.py
@@ -3,7 +3,7 @@ import pygal
 ###
 line_chart = pygal.HorizontalBar(print_labels=True, stack_from_top=False)
 line_chart.title = 'เปรียบเทียบรอยละของประชากรอายุ 15 ปขึ้นไปที่ดื่มสุราหรือเครื่องดื่มมึนเมา จําแนกตามความถี่ในการดื่มสุราหรือเครื่องดื่มมึนเมา ปี 2544 - 2557'
-
+###
 line_chart.add('ดื่มทุกวัน', [{'value': 7.96, 'label': '2544'}, {'value': 9.5, 'label': '2547'}, {'value': 12.81, 'label': '2550'}, {'value': 11.38, 'label': '2554'}, {'value': 9.72, 'label': '2556'}, {'value': 11.12, 'label': '2557'}])
 line_chart.add('5-6 วันต่อสัปดาห์', [{'value': 6.87, 'label': '2554'}, {'value': 7.04, 'label': '2556'}, {'value': 5.12, 'label': '2557'}])
 line_chart.add('3-4 วันต่อสัปดาห์', [{'value': 9.9, 'label': '2544'}, {'value': 10.2, 'label': '2547'}, {'value': 9, 'label': '2550'}, {'value': 8.85, 'label': '2554'}, {'value': 7.73, 'label': '2556'}, {'value': 8.85, 'label': '2557'}])
